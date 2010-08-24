ab4a94e7f76d1fffc13c448a45237c406b13e913
David Haslem <therabidbanana@gmail.com>
2010-08-24 13:22:10 -0400

    Lots of fixes

--- lib/orange-sparkles/sparkles_resource.rb
+++ lib/orange-sparkles/sparkles_resource.rb
@@ -1,15 +1,19 @@
+require 'extlib/mash'
 class SparklesResource < Orange::Resource
   call_me :sparkles
   def stylesheets
-    orange.options["stylesheets"] || []
+    orange.options["sparkles.stylesheets"] || []
   end
   def javascripts
-    orange.options["javascripts"] || []
+    orange.options["sparkles.javascripts"] || []
   end
   def site_name(packet, default = "An Orange Site")
     packet['site'] ? packet['site'].name : default
   end
   def sidebar?
     orange.options["sidebar_on"] || false
+  end
+  def tabs
+    orange.options["sparkles.tabs"].collect{|hash| Mash.new(hash)} || []
   end
 end
